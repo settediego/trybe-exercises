be58c266ee361e4495af9c56ca6af82e32328f90
settediego <dsettedsm@gmail.com>
2022-11-25 23:40:27 +0100

    array 1 to 25 and divisions by 2

--- M01-fundamentos/S04-Intro-JavaScript/exercises/exerciseDay02.js
+++ M01-fundamentos/S04-Intro-JavaScript/exercises/exerciseDay02.js
@@ -69,3 +69,19 @@ if (oddNumbers == 0) {
   console.log("The number of odd values on array is "+ oddNumbers);
 }
 
+// ARRAY 1 TO 25:
+
+let arrayto25 = [1];
+for (index = 0 ; index < 24 ; index += 1) {
+  arrayto25.push (arrayto25[index] + 1);
+}
+console.log(arrayto25);
+
+// DIVISIONS BY 2:
+
+console.log("The last array divided by 2:")
+let div2 = arrayto25
+for (index =0 ; index < div2.length ; index += 1) {
+  div2[index] = div2[index]/2
+}
+console.log(div2)
