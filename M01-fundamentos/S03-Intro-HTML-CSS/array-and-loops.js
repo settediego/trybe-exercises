757dab35df4fe33e1653eb8ec96c0ed4c5d9b350
settediego <dsettedsm@gmail.com>
2022-11-25 21:06:57 +0100

    array and loops structures and example

---
+++ M01-fundamentos/S03-Intro-HTML-CSS/array-and-loops.js
@@ -0,0 +1,27 @@
+const lotteryNumbers = [14, 26, 56, 39, 27, 5];
+
+
+// Imprime os numeros de index 0 a 5 de um em um(não é uma boa prática):
+
+// console.log(lotteryNumbers[0]);
+// console.log(lotteryNumbers[1]);
+// console.log(lotteryNumbers[2]);
+// console.log(lotteryNumbers[3]);
+// console.log(lotteryNumbers[4]);
+// console.log(lotteryNumbers[5]);
+
+// Imprime o número de elementos do array:
+
+// console.log(lotteryNumbers.length);
+
+// Melhor maneira de fazer:
+
+for (let index=0; index < lotteryNumbers.length; index += 1) {
+  console.log(lotteryNumbers[index]);
+}
+
+// Estrutura for(of):
+
+for (value of lotteryNumbers) {
+  console.log(value);
+}
