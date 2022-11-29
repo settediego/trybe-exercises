b8b60cfd0096729ce0068a608ca3fa63e5e0f84d
settediego <dsettedsm@gmail.com>
2022-11-30 00:50:31 +0100

    exercise 04 bigger prime number

--- M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
+++ M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
@@ -24,18 +24,35 @@
 
 // Exercise 03
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let smallerWord = array[0];
-let biggerWord = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let smallerWord = array[0];
+// let biggerWord = array[0];
 
-for (let index = 0 ; index < array.length ; index++) {
-  if (smallerWord.length > array[index].length) {
-    smallerWord = array[index];
+// for (let index = 0 ; index < array.length ; index++) {
+//   if (smallerWord.length > array[index].length) {
+//     smallerWord = array[index];
+//   }
+//   if (biggerWord.length < array[index].length) {
+//     biggerWord = array[index];
+//   }
+// }
+
+// console.log("A maior palavra é " + biggerWord + " e a menor palavra é " + smallerWord);
+
+// Exercise 04
+
+let maiorPrimo = 0;
+
+
+for (let index = 2 ; index <= 50 ; index += 1) {
+  let cont = 0;
+  for (let index2 = index ; index2 > 1 ; index2 -= 1) {
+      if (index%index2 == 0) {  
+        cont += 1;
+      }
+    }
+    if (cont == 1) {     
+    maiorPrimo = index;
+    }
   }
-  if (biggerWord.length < array[index].length) {
-    biggerWord = array[index];
-  }
-}
-
-console.log("A maior palavra é " + biggerWord + " e a menor palavra é " + smallerWord);
-
+console.log(maiorPrimo);
