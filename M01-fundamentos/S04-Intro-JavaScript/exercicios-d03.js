dc1223808eb1021cc10b223ba34e1140166cb5bb
settediego <dsettedsm@gmail.com>
2022-11-29 23:23:35 +0100

    exercise 03 bigger and smaller word

--- M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
+++ M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
@@ -10,14 +10,32 @@
 
 // Exercise 02
 
-let word = 'tryber';
-let drow = '';
+// let word = 'tryber';
+// let drow = '';
 
-for (let index = word.length-1 ; index >= 0 ; index -= 1) {
-  drow += word[index];
-}
+// for (let index = word.length-1 ; index >= 0 ; index -= 1) {
+//   drow += word[index];
+// }
 
 // We can also use the functions below:
 //  drow = word.split("").reverse().join("")
 
-console.log(drow)
+// console.log(drow);
+
+// Exercise 03
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let smallerWord = array[0];
+let biggerWord = array[0];
+
+for (let index = 0 ; index < array.length ; index++) {
+  if (smallerWord.length > array[index].length) {
+    smallerWord = array[index];
+  }
+  if (biggerWord.length < array[index].length) {
+    biggerWord = array[index];
+  }
+}
+
+console.log("A maior palavra é " + biggerWord + " e a menor palavra é " + smallerWord);
+
