c7d86742e3185c2b1eed09478d1da3154b7c7637
settediego <dsettedsm@gmail.com>
2022-11-29 23:01:53 +0100

    exercise 02 backwards words

--- M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
+++ M01-fundamentos/S04-Intro-JavaScript/exercicios-d03.js
@@ -1,7 +1,23 @@
-let num = 10;
-let fat = 1;
+// Exercise 01
 
-for (let index = 1; index <= num ; index += 1) {
-  fat *= index
+// let num = 10;
+// let fat = 1;
+
+// for (let index = 1; index <= num ; index += 1) {
+//   fat *= index
+// }
+// console.log(fat);
+
+// Exercise 02
+
+let word = 'tryber';
+let drow = '';
+
+for (let index = word.length-1 ; index >= 0 ; index -= 1) {
+  drow += word[index];
 }
-console.log(fat);
+
+// We can also use the functions below:
+//  drow = word.split("").reverse().join("")
+
+console.log(drow)
