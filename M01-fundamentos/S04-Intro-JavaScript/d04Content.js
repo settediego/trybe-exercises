8b68f006c857f6497db9691a18f94b248b58fc6a
settediego <dsettedsm@gmail.com>
2022-11-30 19:23:55 +0100

    creates d04Content and put the examples on Trybe Course to test

---
+++ M01-fundamentos/S04-Intro-JavaScript/d04Content.js
@@ -0,0 +1,35 @@
+// Example 1:
+
+let diasDaSemana = {
+  1: 'domingo',
+  2: 'segunda',
+  3: 'terça',
+  4: 'quarta',
+  5: 'quinta',
+  6: 'sexta',
+  7: 'sábado',
+};
+
+diasDaSemana[1];
+console.log(diasDaSemana['1']); // domingo
+
+// Example 2:
+
+let conta = {
+  agencia: '0000',
+  banco: {
+    cod: '012',
+    id: 4,
+    nome: 'TrybeBank',
+  },
+};
+
+let infoDoBanco = 'banco';
+console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+console.log(conta.agencia); // 0000
+console.log(conta['agencia']); // 0000
+
+console.log(conta.banco.cod); // 012
+console.log(conta['banco']['id']); // 4
