b72184aa67cbdd157eca731b72f1024dd565c6ed
settediego <dsettedsm@gmail.com>
2022-11-30 19:43:02 +0100

    adds an example of objects inside strings

--- M01-fundamentos/S04-Intro-JavaScript/d04Content.js
+++ M01-fundamentos/S04-Intro-JavaScript/d04Content.js
@@ -33,3 +33,63 @@ console.log(conta['agencia']); // 0000
 
 console.log(conta.banco.cod); // 012
 console.log(conta['banco']['id']); // 4
+
+// Example 3
+
+let usuario = {
+  id: 99,
+  email: 'jakeperalta@gmail.com',
+  infoPessoal: {
+    nome: 'Jake',
+    sobrenome: 'Peralta',
+    endereco: {
+      rua: 'Smith Street',
+      bairro: 'Brooklyn',
+      cidade: 'Nova Iorque',
+      estado: 'Nova Iorque',
+    },
+  },
+};
+
+console.log(usuario['id']); // 99
+console.log(usuario.email); // jakeperalta@gmail.com
+
+console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+// Exmaple 4
+
+let moradores = [
+  {
+    nome: 'Luiza',
+    sobrenome: 'Guimarães',
+    andar: 10,
+    apartamento: 1005,
+  },
+  {
+    nome: 'William',
+    sobrenome: 'Albuquerque',
+    andar: 5,
+    apartamento: 502,
+  },
+  {
+    nome: 'Murilo',
+    sobrenome: 'Ferraz',
+    andar: 8,
+    apartamento: 804,
+  },
+  {
+    nome: 'Zoey',
+    sobrenome: 'Brooks',
+    andar: 1,
+    apartamento: 101,
+  },
+];
+
+let primeiroMorador = moradores[0];
+console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+console.log(primeiroMorador['andar']); // 10
+
+let ultimoMorador = moradores[moradores.length - 1];
+console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+console.log(ultimoMorador.nome); // Zoey
