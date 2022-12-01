f73603b5a522648ebf9690beb009e73b5222c71f
settediego <dsettedsm@gmail.com>
2022-12-01 15:17:36 +0100

    creating maiorIndex exercise function

---
+++ M01-fundamentos/S04-Intro-JavaScript/maiorIndex.js
@@ -0,0 +1,13 @@
+
+
+function mIndex(array) {
+  let compare = array[0];
+  let maiorIndex;
+  for (let index = 0; index < array.length ; index ++) {
+    if (compare < array[index]) {
+      compare = array[index];
+      maiorIndex = index;
+    }
+  } return maiorIndex;
+}
+console.log(mIndex(exercise = [2,6,26,8,14,7,10]));
