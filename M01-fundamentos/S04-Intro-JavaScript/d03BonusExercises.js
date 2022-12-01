9a4fc54833cf0c349e1099b9f9f3b65f23768358
settediego <dsettedsm@gmail.com>
2022-12-01 01:32:13 +0100

    doing the 1st and 2nd parts of the bonus exercises: printing a square and a right triangle with *

---
+++ M01-fundamentos/S04-Intro-JavaScript/d03BonusExercises.js
@@ -0,0 +1,11 @@
+let n = 5;
+let result = '';
+
+for (let index = 0 ; index < n; index += 1) {
+  result += '*';
+  console.log(result); //Commeting this line, we can make a square of stars
+}
+
+for (let index2 = 0 ; index2 < n ; index2 +=1) {
+console.log(result); //Commeting this line, we can make a right triangle
+}
