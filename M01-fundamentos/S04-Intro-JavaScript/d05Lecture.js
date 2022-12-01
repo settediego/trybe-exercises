b6e25010a622fa2c474530a74ee268ab6fe65b63
settediego <dsettedsm@gmail.com>
2022-12-01 21:29:18 +0100

    putting step by step of how to make an arrow function

--- M01-fundamentos/S04-Intro-JavaScript/d05Lecture.js
+++ M01-fundamentos/S04-Intro-JavaScript/d05Lecture.js
@@ -1,5 +1,24 @@
-let person = 'Rods';
+// Conventional structure
+// function sum (num1, num2) {
+//   return (num1 + num2);
+// }
 
-person = 'Douglas';
 
-console.log(person);
+// Using arrow functions
+// 1. Take off the word FUNCTION and putting CONST instead of it
+
+// const sum (num1, num2) {
+//   return num1 + num2;
+// }
+
+// 2. Putting an = after the name of the function:
+
+// const sum = (num1, num2) {
+//   return num1 + num2;
+// }
+
+// 3. Putting an arrow before the braces:
+
+const sum = (num1, num2) => num1 + num2;
+
+console.log(sum(2,8));
