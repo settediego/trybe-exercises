8afe762ac50dfa0cbb6f66a95d2173a1e5f2e0d1
settediego <dsettedsm@gmail.com>
2023-01-10 01:27:25 +0100

    finishing learning exercises before live class using createElement, appendChild, parentElement, nextElementSibling

--- M01-fundamentos/S05-DOM-Events-WebStorage/d02script2.js
+++ M01-fundamentos/S05-DOM-Events-WebStorage/d02script2.js
@@ -5,7 +5,20 @@ novoIrmao.id = 'novoIrmao';
 pai.appendChild(novoIrmao);
 
 // Crie um filho para elementoOndeVoceEsta.
-
+const OndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const maisUmFilho = document.createElement('section');
+maisUmFilho.id = 'maisUmFilho';
+OndeVoceEsta.appendChild(maisUmFilho);
 
 // Crie um filho para primeiroFilhoDoFilho.
+const primeiroNeto = document.getElementById('primeiroFilhoDoFilho');
+const bisneto = document.createElement('section');
+bisneto.id = 'bisneto';
+primeiroNeto.appendChild(bisneto);
+
 // A partir desse filho criado, acesse terceiroFilho.
+const terceiroFilho = bisneto
+.parentElement
+.parentElement
+.nextElementSibling;
+console.log(terceiroFilho);
