3d4ea0b4dad9bb6f6312d12890af7c19ab99e759
settediego <dsettedsm@gmail.com>
2023-01-10 01:14:32 +0100

    2nd part exercises using the same HTML and trying out tools to create elements on HTML using JS

---
+++ M01-fundamentos/S05-DOM-Events-WebStorage/d02script2.js
@@ -0,0 +1,11 @@
+// Crie um irmão para elementoOndeVoceEsta.
+const pai = document.getElementById('pai');
+const novoIrmao = document.createElement('section');
+novoIrmao.id = 'novoIrmao';
+pai.appendChild(novoIrmao);
+
+// Crie um filho para elementoOndeVoceEsta.
+
+
+// Crie um filho para primeiroFilhoDoFilho.
+// A partir desse filho criado, acesse terceiroFilho.
