246f7be510caff69b2b154783ba8cd9eb44e30db
settediego <dsettedsm@gmail.com>
2023-02-22 17:31:13 +0100

    adding the concept of REST

--- M01-fundamentos/S08-HOFs/d04-content.js
+++ M01-fundamentos/S08-HOFs/d04-content.js
@@ -45,3 +45,16 @@ const fruitSalad = (fruit, additional) => {
 }
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+
+// --------------------- rest ---------------------
+
+// O spread é responsável por espalhar valores, e ele pode ser utilizado para passar parâmetros ao EXECUTAR uma função. Já o rest pode ser utilizado para agrupar valores ao DECLARAR funções que podem receber múltiplos parâmetros.
+
+const countParams = (...args) => {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+
+console.log(countParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(countParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
