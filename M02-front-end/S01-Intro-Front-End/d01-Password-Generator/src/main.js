ec875c090593bd4f9338a4939ade92b05172f009
settediego <dsettedsm@gmail.com>
2023-03-23 23:23:20 +0100

    first lessons about enviroment, Vite, lint settings and package.json. Secure password generator.

--- M02-front-end/S01-Intro-Front-End/d01-Password-Generator/src/main.js
+++ M02-front-end/S01-Intro-Front-End/d01-Password-Generator/src/main.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import { nanoid } from "nanoid";
+import copy from 'clipboard-copy';
+
+import './style.css';
+
+const btnCreate = document.getElementById('btnElement');
+const password = document.getElementById('password');
+btnCreate.addEventListener('click', () => {
+  const safePassword = nanoid()
+  password.innerHTML = safePassword;
+});
+
+password.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
