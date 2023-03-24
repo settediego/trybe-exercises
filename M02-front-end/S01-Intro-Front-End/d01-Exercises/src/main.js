9517442fcfd1b1f92822aaf86874e7a96351efc2
settediego <dsettedsm@gmail.com>
2023-03-24 21:28:08 +0100

    Trybe lint installed and set for the exercise - Done

--- M02-front-end/S01-Intro-Front-End/d01-Exercises/src/main.js
+++ M02-front-end/S01-Intro-Front-End/d01-Exercises/src/main.js
@@ -7,17 +7,19 @@ const validateBtn = document.getElementById('btnValidation');
 const seletor = document.getElementById('option');
 const outputText = document.getElementById('answer');
 
+const UUID_VERSION = 4;
+
 validateBtn.addEventListener('click', (event) => {
-  //Usado para evitar que ao clicar no botão a página seja regarregada
+  // Usado para evitar que ao clicar no botão a página seja regarregada
   event.preventDefault();
 
   const inputType = {
     email: validator.isEmail(valueInput.value),
     CPF: validator.isTaxID(valueInput.value, 'pt-BR'),
     URL: validator.isURL(valueInput.value),
-    CPF: validator.isUUID(valueInput.value, 4),
-    CPF: validator.isHexColor(valueInput.value),
-  }
+    uuid: validator.isUUID(valueInput.value, UUID_VERSION),
+    hexColor: validator.isHexColor(valueInput.value),
+  };
 
   outputText.innerHTML = `A validação retornou ${inputType[seletor.value]}`;
 });
