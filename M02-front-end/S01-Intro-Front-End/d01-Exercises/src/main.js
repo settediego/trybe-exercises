6778f54c24678ed8c27d8348cffd139172be4295
settediego <dsettedsm@gmail.com>
2023-03-24 21:14:50 +0100

    creating all the JS code for the exercise

--- M02-front-end/S01-Intro-Front-End/d01-Exercises/src/main.js
+++ M02-front-end/S01-Intro-Front-End/d01-Exercises/src/main.js
@@ -1,2 +1,23 @@
 import validator from 'validator';
 
+import './style.css';
+
+const valueInput = document.getElementById('value');
+const validateBtn = document.getElementById('btnValidation');
+const seletor = document.getElementById('option');
+const outputText = document.getElementById('answer');
+
+validateBtn.addEventListener('click', (event) => {
+  //Usado para evitar que ao clicar no botão a página seja regarregada
+  event.preventDefault();
+
+  const inputType = {
+    email: validator.isEmail(valueInput.value),
+    CPF: validator.isTaxID(valueInput.value, 'pt-BR'),
+    URL: validator.isURL(valueInput.value),
+    CPF: validator.isUUID(valueInput.value, 4),
+    CPF: validator.isHexColor(valueInput.value),
+  }
+
+  outputText.innerHTML = `A validação retornou ${inputType[seletor.value]}`;
+});
