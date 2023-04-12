ab599dcf135b913c2730a2e3e9a363502cb52243
settediego <dsettedsm@gmail.com>
2023-04-12 21:48:42 +0200

    lectures notes and content about react.

--- M02-front-end/S02-Intro-React/hello-world-react/src/App.js
+++ M02-front-end/S02-Intro-React/hello-world-react/src/App.js
@@ -9,4 +9,13 @@ class App extends React.Component {
   }
 }
 
+// React é uma biblioteca
+// serve para criar interfaces
+// cria componentes com JSX para criar interface
+// Todo componente no React é com letra maiúscula
+// Dentro do componente React temos o JSX(que parece com HTML)
+// .js e .jsx são a mesma coisa
+// para colocar código JS no JSX usamos o { variavel }
+// 2 formas de criar os componentes: função e outra com classes
+
 export default App;
--- M02-front-end/S02-Intro-React/hello-world-react/src/index.js
+++ M02-front-end/S02-Intro-React/hello-world-react/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// Une o Componente App com a index.html
+// NÃO MODIFICAR ESSE ARQUIVO
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
