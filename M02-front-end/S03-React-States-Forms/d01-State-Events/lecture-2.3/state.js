6e2735a5df11fe57f6cb17f7fe21ab6184cbde21
settediego <dsettedsm@gmail.com>
2023-05-18 01:08:50 +0200

    creating state.js with an example of state declaration and how to change it

---
+++ M02-front-end/S03-React-States-Forms/d01-State-Events/lecture-2.3/state.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numeroDeCliques: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
+    }));
+  }
+
+  render() {
+    const { numeroDeCliques } = this.state;
+    return (
+      <button
+        type="button"
+        onClick={ this.handleClick }
+      >
+        { numeroDeCliques }
+      </button>
+    );
+  }
+}
+
+export default App;
