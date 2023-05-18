77ff8b16e6fe52cbd8ca119ca2a3e332f4fa7f4c
settediego <dsettedsm@gmail.com>
2023-05-18 02:12:23 +0200

    making all the steps of the section exercise. Using state and changing backgroundColor based on the state.

--- M02-front-end/S03-React-States-Forms/d01-State-Events/exercise/src/App.js
+++ M02-front-end/S03-React-States-Forms/d01-State-Events/exercise/src/App.js
@@ -36,22 +36,77 @@ class App extends Component {
 
   // Podemos usar da forma como eu fiz acima ou da forma como está abaixo(parte antes do render).
 
+
+  // Na parte 3 do exercício foi solicitado que eu colocasse estado nos botões e alterasse eles com os cliques.
+  // Mantive as arrow functions na declaração das funções, e usei a Public Class Fields para a declaração do STATE, feito nas linhas abaixo.
+
+  state = {
+    firstBtn: 0,
+    secondBtn: 0,
+    thirdBtn: 0,
+  };
+  
+  // Na parte 4 do exercício preciso adicionar lógica para que um console.log diga a cor do botão e que ele fique verde qnd o número de cliques for par.
+
+  getButtonColor = (num) => {
+    return num % 2 === 0 ? 'green' : 'white';
+  };
+
+  // Nas funções adicionei a função setState para alterar de forma dinâmica o estado de cada botão ao ser clicado.
+
   handleClick1 = () => {
-    console.log('1st btn "this"', this);
+    const { firstBtn } = this.state;
+    this.setState((initialValue, _props) => ({
+      firstBtn: initialValue.firstBtn + 1,
+    }), () => {
+      console.log(`First button color ${this.getButtonColor(firstBtn)}`)
+      });
   }
   handleClick2 = () => {
-    console.log('2nd btn "this"', this);
+    const { secondBtn } = this.state;
+    this.setState((initialValue, _props) => ({
+      secondBtn: initialValue.secondBtn + 1,
+    }), () => {
+      console.log(`Second button color ${this.getButtonColor(secondBtn)}`)
+    });
   }
   handleClick3 = () => {
-    console.log('3rd btn "this"', this);
+    const { thirdBtn } = this.state;
+    this.setState((initialValue, _props) => ({
+      thirdBtn: initialValue.thirdBtn + 1,
+    }), () => {
+      console.log(`Third button color ${this.getButtonColor(thirdBtn)}`)
+    });
   }
   
+  // No render, chamei os estados de cada botão utilizando as chaves do objeto THIS, na chave STATE, buscando a chave referente ao numero de cliques de cada
+  // botão, registrando a quantidade de cliques a partir das funções criadas acima.
+
   render() {
+    const { firstBtn, secondBtn, thirdBtn } = this.state;
     return (
       <div>
-        <button onClick={this.handleClick1}>1st btn</button>
-        <button onClick={this.handleClick2}>2nd btn</button>
-        <button onClick={this.handleClick3}>3rd btn</button>
+        <button
+          type="button"
+          onClick={this.handleClick1}
+          style={ { backgroundColor: this.getButtonColor(firstBtn) } }
+        >
+            1st btn | number of clicks: {this.state.firstBtn}
+        </button>
+        <button 
+          type="button"
+          onClick={this.handleClick2}
+          style={ { backgroundColor: this.getButtonColor(secondBtn) } }
+        >
+            2nd btn | number of clicks: {this.state.secondBtn}
+        </button>
+        <button 
+          type="button"
+          onClick={this.handleClick3}
+          style={ { backgroundColor: this.getButtonColor(thirdBtn) } }
+        >
+            3rd btn | number of clicks: {this.state.thirdBtn}
+        </button>
       </div>
     );
   }
