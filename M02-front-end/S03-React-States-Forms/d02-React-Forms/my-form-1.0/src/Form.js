5ae84c94bc906ff89cadac28c141bb40cc72bc3f
settediego <dsettedsm@gmail.com>
2023-05-20 00:40:26 +0200

    it does the exercise of the day using generic event handlers, input file and fieldset

--- M02-front-end/S03-React-States-Forms/d02-React-Forms/my-form-1.0/src/Form.js
+++ M02-front-end/S03-React-States-Forms/d02-React-Forms/my-form-1.0/src/Form.js
@@ -5,19 +5,23 @@ class Form extends Component {
   constructor() {
     super();
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    
-    this.state = {
-      país: '',
-      nomeCompleto: '',
-      idade: 0,
-      ultimoAno: ''
-    }
+    this.handleChange = this.handleChange.bind(this);
+
   }
 
-  handleNameChange(event) {
+  state = {
+    país: '',
+    nomeCompleto: '',
+    idade: 0,
+    ultimoAno: '',
+    termos: false,
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      nomeCompleto: event.target.value,
+      [name]: value,
     });
   }
 
@@ -26,30 +30,62 @@ class Form extends Component {
       <div>
         <h1>Exercício para aula de React e Componentes Controlados</h1>
         <form className="form">
-          <label>
-            Escolha o seu país:
-            <select>
-              <option value="Brasil">Brasil</option>
-              <option value="Outro">Outro</option>
-            </select>
-          </label>
+          <fieldset>
+            <legend>Informações pessoais</legend>
 
-          <label>
-            Nome Completo: 
-            <input
-              type='text'
-              value={ this.state.nomeCompleto }
-              onChange={ this.handleNameChange }
-            />
-          </label>
+            <label>
+              Escolha o seu país:
+              <select
+                name='país'
+                value={this.state.país}
+                onChange={this.handleChange}
+              >
+                <option value="Brasil">Brasil</option>
+                <option value="Outro">Outro</option>
+              </select>
+            </label>
 
-          <label>
-            Idade: <input type='number' />
-          </label>
+            <label>
+              Nome Completo:
+              <input
+                name='nomeCompleto'
+                type='text'
+                value={this.state.nomeCompleto}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              Idade:
+              <input
+                name='idade'
+                type='number'
+                value={this.state.idade}
+                onChange={this.handleChange}
+              />
+            </label>
+          </fieldset>
 
           <label>
             Conte aqui como foi o seu último ano:
-            <textarea name="últimoAno" rows={4} cols={40} />
+            <textarea
+              name="ultimoAno"
+              rows={4}
+              cols={40}
+              value={this.state.ultimoAno}
+              onChange={this.handleChange}
+            />
+          </label>
+
+            <input type='file' />
+          <label>
+            Concordo com os termos
+            <input
+              name='termos'
+              type='checkbox'
+              value={this.state.termos}
+              onChange={this.handleChange}
+            />
           </label>
         </form>
       </div>
